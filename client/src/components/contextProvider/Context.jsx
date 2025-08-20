a1dc94e25e032aed4cea5e0cf956a88d50a05601
Ronaldo Nandeibam <ronaldonandeibam7@gmail.com>
2025-08-20 07:17:20 +0530

    Update Context.jsx

--- client/src/components/contextProvider/Context.jsx
+++ client/src/components/contextProvider/Context.jsx
@@ -8,7 +8,7 @@ export const FinancialRecordsProvider = ({ children }) => {
   const { user, isLoaded } = useUser();
 
   // Base API URL for local development
-  const API_URL = process.env.REACT_APP_API_URL;
+  const API_URL = "https://finance-track-dqer.onrender.com";
 
   // Fetch records when user is loaded
   const fetchRecord = async () => {
